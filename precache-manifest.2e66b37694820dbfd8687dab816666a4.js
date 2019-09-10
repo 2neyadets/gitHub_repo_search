dfcf05d4d0bf3caf6cd286a5dd438a3b1ed82b19
Alexander Sintsov <avsintsov91@mail.ru>
2019-09-10 17:59:43 +0700

    Updates

---
+++ precache-manifest.2e66b37694820dbfd8687dab816666a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "217785e6112520dddd29d5798907d913",
+    "url": "/gitHub_repo_search/static/media/preloader.217785e6.gif"
+  },
+  {
+    "revision": "eac99e47fff05056c878",
+    "url": "/gitHub_repo_search/static/js/runtime~main.51d543bf.js"
+  },
+  {
+    "revision": "ab3e15a91c51276a7fc8",
+    "url": "/gitHub_repo_search/static/js/main.ca0797fb.chunk.js"
+  },
+  {
+    "revision": "738bd527c44d502363de",
+    "url": "/gitHub_repo_search/static/js/2.28fae2bd.chunk.js"
+  },
+  {
+    "revision": "ab3e15a91c51276a7fc8",
+    "url": "/gitHub_repo_search/static/css/main.f64d6c75.chunk.css"
+  },
+  {
+    "revision": "1eac0ebce72b91f23740ed1b4f43dfee",
+    "url": "/gitHub_repo_search/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gitHub_repo_search/precache-manifest.2e66b37694820dbfd8687dab816666a4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gitHub_repo_search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
